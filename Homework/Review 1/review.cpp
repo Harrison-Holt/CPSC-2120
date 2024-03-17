73239548bc46c66d47b90a29804551c86a68ba3f
Harrison Holt <116909429+Harrison-Holt@users.noreply.github.com>
2024-03-17 16:03:11 -0400

    Create  review.cpp

---
+++ Homework/Review 1/review.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std; 
+
+ void ReadStdIn() {
+
+    int integer; 
+    double floating_number; 
+    string str; 
+    
+    
+    cin >> integer; 
+    cin >> floating_number; 
+    cin >> str; 
+
+    cout << integer << endl << floating_number << endl << str << endl; 
+}
